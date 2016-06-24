469c70d4e2dc393e62a4987e27ede5a839fcd10e
JustinNick <2441178626@qq.com>
2016-06-24 16:37:15 +0800

    JustinNick初次导入到Github中

---
+++ app/src/androidTest/java/com/example/johncheng/day30_9gonggeimage/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.example.johncheng.day30_9gonggeimage;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
